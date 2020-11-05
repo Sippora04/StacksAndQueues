f7ada947f5f6207bb27a37f25da93dcf1377d552
Sippora04 <sippora.toppo95@gmail.com>
2020-11-05 07:31:27 +0530

    Added: dequeue

--- test/java/com/stacksandqueues/MyQueueTest.java
+++ test/java/com/stacksandqueues/MyQueueTest.java
@@ -18,4 +18,20 @@ public class MyQueueTest {
 		myQueue.printQueue();
 		assertEquals(myFirstNode, myNode);
 	}
+	
+	@Test
+	public void given3Numbers_WhenDequeueRemoveFromTheBegining() {
+		MyQueue<Integer> myQueue = new MyQueue<>();
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myQueue.enqueu(myFirstNode);
+		myQueue.enqueu(mySecondNode);
+		myQueue.enqueu(myThirdNode);
+		INode<Integer> myNode = myQueue.peak();
+		assertEquals(myFirstNode, myNode);
+		INode<Integer> popedNode = myQueue.dequeue();
+		myQueue.printQueue();
+		assertEquals(myFirstNode, popedNode);
+	}
 }
